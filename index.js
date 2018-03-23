fe92264991e57182d2ca7fc198648ba8bec3c13b
Steve Krenek <steve.krenek@zymo.io>
2018-03-23 13:52:47 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,25 @@
+const prompt = require('prompt');
+const childProc = require('child_process');
+
+prompt.start();
+
+console.log(`1) Add a user
+q) Quit
+
+Enter an option:`);
+
+prompt.get(['choice'], (err, result) => {
+  switch (result.choice) {
+    case '1':
+      prompt.get(['username'], (err, ur) => {
+        childProc.execSync(`sudo useradd -G sudo,ubuntu,dev,ssh -m -s /bin/bash ${username}`);
+      });
+      break;
+    default:
+      process.exit(0);
+  }
+});
+
+process.on('exit', (code) => {
+  console.log('. Done .');
+})
